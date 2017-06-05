32712a99aed1de16e1092a5b2bc1b5645d44dbf6
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-06-05 23:54:22 +0200

    add search only one user

--- src/main/java/com/akademiakodu/RestTest.java
+++ src/main/java/com/akademiakodu/RestTest.java
@@ -10,15 +10,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Created by Lukasz Kolacz on 05.06.2017.
  */
 public class RestTest {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwę użytkownika:");
+        String username = scanner.next();
+
 //        OTWIERAMY POLACZENIE HTTP
         try {
-            URL url = new URL("http://localhost:8080/api/user");
+//            URL url = new URL("http://localhost:8080/api/user");
+            URL url = new URL("http://localhost:8080/api/user/" + username);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.setRequestProperty("Access-Password", "akademiakodujestfajna");
@@ -28,26 +35,33 @@ public class RestTest {
             StringBuilder stringBuilder = new StringBuilder();
 
             String line = null;
-            while ((line=reader.readLine())!= null){
+            while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line + "\n");
             }
             System.out.println(stringBuilder.toString());
 
-//            Tworzenie tablicy obiektów JSON-owskich
-                JSONArray array = new JSONArray(stringBuilder.toString());
-                for(int i=0; i<array.length(); i++){
-                    JSONObject object = array.getJSONObject(i);
-                    System.out.println("Username: " + object.getString("username"));
-                    System.out.println("Password: " + object.getString("password"));
-                    System.out.println("--------------------------------------------");
+//            Tworzenie tablicy obiektów JSON-owskich:
 
-                }
+//            JSONArray array = new JSONArray(stringBuilder.toString());
+//            for (int i = 0; i < array.length(); i++) {
+//                JSONObject object = array.getJSONObject(i);
+//                System.out.println("Username: " + object.getString("username"));
+//                System.out.println("Password: " + object.getString("password"));
+//                System.out.println("--------------------------------------------");
+//
+//            }
+//            ---------------------------------------------------------------------------
+//            Wyszukiwanie tylko jednego usera po jego nazwie
+            JSONObject object = new JSONObject(stringBuilder.toString());
+            System.out.println("Username: " + object.getString("username"));
+            System.out.println("Password: " + object.getString("password"));
+            System.out.println("--------------------------------------------");
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
