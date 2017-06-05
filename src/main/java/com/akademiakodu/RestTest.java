88d3192e03769d13e2db3b871372c7279fc974c0
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-06-05 20:09:29 +0200

    Rest exercise

---
+++ src/main/java/com/akademiakodu/RestTest.java
@@ -0,0 +1,54 @@
+package com.akademiakodu;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Lukasz Kolacz on 05.06.2017.
+ */
+public class RestTest {
+    public static void main(String[] args) {
+//        OTWIERAMY POLACZENIE HTTP
+        try {
+            URL url = new URL("http://localhost:8080/api/user");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Access-Password", "akademiakodujestfajna");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+            while ((line=reader.readLine())!= null){
+                stringBuilder.append(line + "\n");
+            }
+            System.out.println(stringBuilder.toString());
+
+//            Tworzenie tablicy obiektów JSON-owskich
+                JSONArray array = new JSONArray(stringBuilder.toString());
+                for(int i=0; i<array.length(); i++){
+                    JSONObject object = array.getJSONObject(i);
+                    System.out.println("Username: " + object.getString("username"));
+                    System.out.println("Password: " + object.getString("password"));
+                    System.out.println("--------------------------------------------");
+
+                }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
